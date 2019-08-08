0cdbbc17578582429b372e1cbd32f594808d07e8
Mike "Xodiv" Battersby <mib@post.com>
2019-08-08 19:36:08 +1000

    Only LOOT_CLOSED

--- NoLootFrame.lua
+++ NoLootFrame.lua
@@ -14,6 +14,19 @@ local events = {
     "LOOT_READY",
 }
 
+local function AnnounceLoot(info)
+    if info and info.quantity > 0 and info.quality > 0 then
+        local colorInfo = ITEM_QUALITY_COLORS[info.quality]
+        local color = CreateColor(colorInfo.r, colorInfo.g, colorInfo.b, colorInfo.a)
+        local txt = color:WrapTextInColorCode('[' .. info.item .. ']')
+            txt = txt
+                .. '|cff19a919'
+                .. format('x%d', info.quantity or 0)
+                .. FONT_COLOR_CODE_CLOSE
+            UIErrorsFrame:AddMessage(txt)
+    end
+end
+
 -- AutoLoot is done server-side now, so all we have to do is handle
 -- the events so the window doesn't show
 
@@ -29,28 +42,19 @@ function NoLootFrame_OnEvent(self, event, ...)
     end
 
     if not self.autoLoot then
-        LootFrame_OnEvent(LootFrame, event, ...)
+        if not InCombatLockdown() then
+            LootFrame_OnEvent(LootFrame, event, ...)
+        end
         return
     end
 
-    if event == 'LOOT_SLOT_CLEARED' then
-        local slot = ...
-        local info = self.lootInfo[slot]
-        if info and info.quantity > 0 and info.quality > 0 then
-            local colorInfo = ITEM_QUALITY_COLORS[info.quality]
-            local color = CreateColor(colorInfo.r, colorInfo.g, colorInfo.b, colorInfo.a)
-            local txt = color:WrapTextInColorCode('[' .. info.item .. ']')
-                txt = txt
-                    .. '|cff19a919'
-                    .. format('x%d', info.quantity or 0)
-                    .. FONT_COLOR_CODE_CLOSE
-                UIErrorsFrame:AddMessage(txt)
-            self.lootInfo[slot] = nil
-        end
-    end
-
     if event == 'LOOT_CLOSED' then
-        self.lootInfo = nil
+        if self.lootInfo then
+            for _, info in ipairs(self.lootInfo or {}) do
+                AnnounceLoot(info)
+            end
+            self.lootInfo = nil
+        end
     end
 end
 
